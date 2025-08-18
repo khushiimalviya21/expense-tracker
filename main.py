2fd0a38c4011b0b5124ea6e61996e930e25f955a
khushi <khushimalviya61@gmail.com>
2025-08-18 22:34:27 +0530

    Added modify file

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ class Expense(BaseModel):
     id: int
     title: str
     amount: float
-    date: Optional[datetime] = datetime.now()  # Auto-filled if not provided
+    date: Optional[datetime] = None  # Will accept a date from frontend
 
 # Jinja2 Templates directory
 templates = Jinja2Templates(directory="templates")
@@ -28,6 +28,9 @@ def read_root(request: Request):
 # Add a new expense
 @app.post("/expenses")
 def add_expense(expense: Expense):
+    # If date is not provided, set it to now
+    if not expense.date:
+        expense.date = datetime.now()
     expenses.append(expense)
     return {"message": "Expense added"}
 
@@ -45,17 +48,20 @@ def delete_expense(expense_id: int):
 
 # Get monthly total for a given user ID
 @app.get("/expenses/summary/{user_id}")
-def get_monthly_total(user_id: int):
-    now = datetime.now()
+def get_monthly_total(user_id: int, month: Optional[int] = None, year: Optional[int] = None):
     total = 0.0
 
     for e in expenses:
-        if e.id == user_id and e.date.month == now.month and e.date.year == now.year:
-            total += e.amount
+        if e.id == user_id:
+            e_month = e.date.month
+            e_year = e.date.year
+
+            if (month is None or month == e_month) and (year is None or year == e_year):
+                total += e.amount
 
     return {
         "user_id": user_id,
-        "month": now.month,
-        "year": now.year,
+        "month": month if month else "all",
+        "year": year if year else "all",
         "total": total
     }
