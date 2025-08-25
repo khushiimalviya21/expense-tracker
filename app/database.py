7226f57857eab607eef0f6c61ca036bc46765c5c
khushiimalviya21 <khushimalviya61@gmail.com>
2025-08-25 14:37:15 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from databases import Database
 from sqlalchemy import create_engine, MetaData
 
-DATABASE_URL = "sqlite:///./expenses.db"
+DATABASE_URL = "sqlite:////app/data/expenses.db"
 
 database = Database(DATABASE_URL)
 engine = create_engine(DATABASE_URL)
